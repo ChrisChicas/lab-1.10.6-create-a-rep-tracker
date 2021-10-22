30edc215e7394ba51b0705db1fa03e78567f2df0
Chris Chicas <christianchicas7@gmail.com>
2021-10-22 00:57:10 -0400

    index.js written

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your Code Here
+let repstore = localStorage.getItem("reps");
+let reps = null
+if(repstore == null) {
+    reps = []
+} else {
+    reps = JSON.parse(repstore)
+};
+
+let repin = window.prompt("Enter the number of reps: ");
+reps.push(repin);
+
+repstore = JSON.stringify(reps);
+localStorage.setItem("reps", repstore);
+
+renderChart(reps);
