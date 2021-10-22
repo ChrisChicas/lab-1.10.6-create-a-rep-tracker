3ce6daf223a120de0168f5d96f57473f255a37f3
Chris Chicas <christianchicas7@gmail.com>
2021-10-22 05:51:22 -0400

    index.js revised and added multiple charts for different graphs.

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
-let repstore = localStorage.getItem("reps");
-let reps = null
-if(repstore == null) {
-    reps = []
+let verify = localStorage.getItem("pushreps");
+let pushreps = null
+if(verify == null) {
+    pushreps = []
 } else {
-    reps = JSON.parse(repstore)
+    pushreps = JSON.parse(verify)
 };
 
-let repin = window.prompt("Enter the number of reps: ");
-reps.push(repin);
+let pushrepin = window.prompt("Enter the number of pushup reps: ");
+pushreps.push(pushrepin);
 
-repstore = JSON.stringify(reps);
-localStorage.setItem("reps", repstore);
+let pushrepstore = JSON.stringify(pushreps);
+localStorage.setItem("pushreps", pushrepstore);
 
-renderChart(reps);
+renderChart(pushreps, "Pushup ");
+
+let sitrepin = localStorage.getItem("sitreps");
+let sitreps = null
+if(sitrepin == null) {
+    sitreps = []
+} else {
+    sitreps = JSON.parse(sitrepin)
+};
+
+sitrepin = window.prompt("Enter the number of situp reps: ");
+sitreps.push(sitrepin);
+
+let sitrepstore = JSON.stringify(sitreps);
+localStorage.setItem("sitreps", sitrepstore);
+
+renderChart(sitreps, "Situp ");
+
+let squatrepstore = localStorage.getItem("squatreps");
+let squatreps = null
+if(squatrepstore == null) {
+    squatreps = []
+} else {
+    squatreps = JSON.parse(squatrepstore)
+};
+
+let squatrepin = window.prompt("Enter the number of squat reps: ");
+squatreps.push(squatrepin);
+
+squatrepstore = JSON.stringify(squatreps);
+localStorage.setItem("squatreps", squatrepstore);
+
+renderChart(squatreps, "Squat ");
+
+let laprepstore = localStorage.getItem("lapreps");
+let lapreps = null
+if(laprepstore == null) {
+    lapreps = []
+} else {
+    lapreps = JSON.parse(laprepstore)
+};
+
+let laprepin = window.prompt("Enter the number of laps run: ");
+lapreps.push(laprepin);
+
+laprepstore = JSON.stringify(lapreps);
+localStorage.setItem("lapreps", laprepstore);
+
+renderChart(lapreps, "Laps ");
